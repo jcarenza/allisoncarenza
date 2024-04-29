38e0ceea3847dc841da663e48801ffbd9e55108c
Jon Carenza <jcarenza@gmail.com>
2024-04-29 17:17:42 -0500

    pages url config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import preact from "@astrojs/preact"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://jcarenza.github.io/allisoncarenza",
+    site: "https://jcarenza.github.io",
+    base: "/allisoncarenza",
     // Enable the Preact integration to support Preact JSX components.
     integrations: [preact()],
 })
